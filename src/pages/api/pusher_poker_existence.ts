7ccd60bf15909c98b2eaad32584247f96173e402
JeremyMoeglich <jeremy@moeglich.dev>
2023-04-28 07:08:50 +0200

    changes

--- src/pages/api/pusher_poker_existence.ts
+++ src/pages/api/pusher_poker_existence.ts
@@ -1,9 +1,15 @@
-import { NextApiHandler } from "next";
+import type { NextApiHandler } from "next";
+
 
 const handler: NextApiHandler = (req, res) => {
-    console.log("pusher_poker_existence", JSON.stringify(req.body.body));
+    if (req.method !== 'POST') {
+        res.status(405).send({ message: 'Only POST requests allowed' })
+        return
+    }
 
-    res.status(200).json({ status: "ok" });
+    console.log(JSON.stringify(req.body));
+
+    res.status(200).send('Success');
 };
 
 export default handler;
